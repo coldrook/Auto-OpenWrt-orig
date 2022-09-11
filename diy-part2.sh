2c05bb49229aac6b797ed8ed3bd4b4e71a517423
coldrook <8768561+coldrook@users.noreply.github.com>
2022-09-12 07:04:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,21 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+##补充汉化       
+cp -r -f ./feeds/xiangfeidexiaohuo/files/udpxy.lua ./feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi
+
+##fixed mosdns
+rm -rf ./feeds/packages/net/mosdns
+cp -r -f ./feeds/xiangfeidexiaohuo/op-mosdns ./feeds/packages/net/mosdns
+
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+
+##加入作者信息
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt.orig@autocc-$(date +%Y%m%d)'/g" package/lean/default-settings/files/zzz-default-settings
+
+
+#sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.19/g' target/linux/x86/Makefile
+
+# welcome test
