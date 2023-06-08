2f31130ea777932541dcac2f7c0a2592a59099b5
coldrook <8768561+coldrook@users.noreply.github.com>
2023-06-08 19:45:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,18 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Docker v20.10.17
-# rm -rf feeds/packages/utils/dockerd
-# rm -rf feeds/packages/utils/containerd
-# rm -rf feeds/packages/utils/runc
-# rm -rf feeds/packages/utils/libnetwork
-# rm -rf feeds/packages/utils/tini
-# svn export https://github.com/coolsnowwolf/packages/trunk/utils/docker feeds/packages/utils/docker
-# svn export https://github.com/coolsnowwolf/packages/trunk/utils/dockerd feeds/packages/utils/dockerd
-# svn export https://github.com/coolsnowwolf/packages/trunk/utils/containerd feeds/packages/utils/containerd
-# svn export https://github.com/coolsnowwolf/packages/trunk/utils/runc feeds/packages/utils/runc
-# svn export https://github.com/coolsnowwolf/packages/trunk/utils/libnetwork feeds/packages/utils/libnetwork
-# svn export https://github.com/coolsnowwolf/packages/trunk/utils/tini feeds/packages/utils/tini
+svn export https://github.com/coolsnowwolf/luci/trunk/libs/luci-lib-fs ./package/add/luci-lib-fs
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-filetransfer ./package/add/luci-app-filetransfer
+sed -i "s/\.\.\/\.\./\$\(TOPDIR\)\/feeds\/luci/g" ./package/add/luci-app-filetransfer/Makefile
+cp -af ./package/add/luci-app-filetransfer/po/zh-cn  ./package/add/luci-app-filetransfer/po/zh_Hans
 
 ##补充汉化       
 #cp -r -f ./feeds/xiangfeidexiaohuo/files/udpxy.lua #./feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi
